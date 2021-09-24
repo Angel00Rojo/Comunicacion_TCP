e788feed64bdb5506923befd2d43ba7c1fb9b8ba
Angel00Rojo <88450883+Angel00Rojo@users.noreply.github.com>
2021-09-24 11:45:19 -0500

    Update servidorTCP.py

--- servidorTCP.py
+++ servidorTCP.py
@@ -3,3 +3,7 @@ import sys
 
 #Se crea el Conector TCP/IP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+TCP_IP = '127.0.0.1' 
+TCP_PORT = 5005 #Puedes poner otro, verifica que no lo uses ya y que sea el mismo en el cliente tcp. 
+BUFFER_SIZE = 2048 
